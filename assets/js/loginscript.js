1278c61aa35baf939830db5976fae6506aa8b11c
Iury Rodrigues de Godoy <iuryrodriguesgodoy18@gmail.com>
2025-02-08 00:59:42 -0300

    Novas alterações squad 03

--- assets/js/loginscript.js
+++ assets/js/loginscript.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menu-button");
-    const menuIcon = document.getElementById("menu-icon");
-
-    menuButton.addEventListener("click", function () {
-        if (menuIcon.classList.contains("fa-bars")) {
-            menuIcon.classList.remove("fa-bars");
-            menuIcon.classList.add("fa-times");
-        } else {
-            menuIcon.classList.remove("fa-times");
-            menuIcon.classList.add("fa-bars");
-        }
-    });
-});
-
-
 async function carregarFormulario(url, seletorDestino) {
     try {
 
@@ -39,4 +23,4 @@ async function carregarFormulario(url, seletorDestino) {
     }
 }
 
-carregarFormulario("outra_pagina.html", "#cadastro.html");
+carregarFormulario("outra_pagina.html", "#login.html");
--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,31 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+async function carregarFormulario(url, seletorDestino) {
+    try {
+        const resposta = await fetch(url);
+        if (!resposta.ok) {
+            throw new Error("Erro ao carregar o formulário.");
+        }
+
+        const html = await resposta.text();
+
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, "text/html");
+
+        const formulario = doc.querySelector("form");
+
+        if (formulario) {
+            document.querySelector(seletorDestino).innerHTML = formulario.outerHTML;
+        } else {
+            console.error("Nenhum formulário encontrado na página.");
+        }
+    } catch (erro) {
+        console.error("Erro:", erro.message);
+    }
+}
+
+carregarFormulario("outra_pagina.html", "#login.html");
+
+
